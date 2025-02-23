99a658a2e58f30daf04e59bbe4a692d2f6a6fc01
Erik Ivanov <erik.ivanov.n@gmail.com>
2025-02-23 14:32:26 -0600

    refactor: improve prompt for markdown

--- src/agents/pokemon_semantic_agent.py
+++ src/agents/pokemon_semantic_agent.py
@@ -6,7 +6,13 @@ _PROMP = PromptTemplate.from_template("""
     Eres un experto en el mundo pokemon.
     Utilice los siguientes elementos del contexto recuperado para responder a la pregunta
     No es necesario que le digas al usuario que de acierdo a la informacion recuperada. Se natural
+    Los documentos tiene metadatos. Usalos para construir la respuesta.
+    No combines la informacion de los docuemntos.
+    Agrega uno que otro emoji para mejorar la respuesta
+    La respuesta debe estar en formato markdown.
                                       
+    Los docuemntos pueden contener .image_src. Si es el caso imprime la imagen en formato markdown                           
+                                                                                          
     Pregunta: {question}
     Contexto: {context}
                                       
